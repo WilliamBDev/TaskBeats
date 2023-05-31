7f329904fdef5ee37a0947124ac245efb83b922e
WilliamBDev <william.barbosa.dev@gmail.com>
2023-05-31 06:22:06 -0300

    create viewmodel

--- app/src/main/java/com/comunidadedevspace/taskbeats/presentation/TaskListActivity.kt
+++ app/src/main/java/com/comunidadedevspace/taskbeats/presentation/TaskListActivity.kt
@@ -10,19 +10,14 @@ import android.view.View
 import android.widget.LinearLayout
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.RecyclerView
-import androidx.room.Room
 import com.comunidadedevspace.taskbeats.R
-import com.comunidadedevspace.taskbeats.data.AppDataBase
 import com.comunidadedevspace.taskbeats.data.Task
 import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers.IO
-import kotlinx.coroutines.launch
 import java.io.Serializable
 
-class MainActivity : AppCompatActivity() {
+class TaskListActivity : AppCompatActivity() {
 
     private lateinit var ctnContent: LinearLayout
 
@@ -31,34 +26,22 @@ class MainActivity : AppCompatActivity() {
         TaskListAdapter(::onListItemClicked)
     }
 
-    //banco de dados
-    private val dataBase by lazy {
-        Room.databaseBuilder(
-            applicationContext,
-            AppDataBase::class.java, "taskbeats-database"
-        ).build()
-    }
-
-    private val dao by lazy {
-        dataBase.taskDao()
+    // viewModel
+    private val viewModel: TaskListViewModel by lazy {
+        TaskListViewModel.create(application)
     }
 
 
     private val starForResult = registerForActivityResult(
         ActivityResultContracts.StartActivityForResult()
     ) { result: ActivityResult ->
-        if (result.resultCode == Activity.RESULT_OK){
+        if (result.resultCode == Activity.RESULT_OK) {
 
             // pegando resultado
             val data = result.data
             val taskAction = data?.getSerializableExtra(TASK_ACTION_RESULT) as TaskAction
-            val task: Task = taskAction.task
 
-            when (taskAction.ActionType) {
-                ActionType.DELETE.name ->  deleteById(task.id)
-                ActionType.CREATE.name ->  insertIntoDataBase(task)
-                ActionType.UPDATE.name ->  updateIntoDataBase(task)
-            }
+            viewModel.execute(taskAction)
         }
     }
 
@@ -66,9 +49,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_task_list)
         setSupportActionBar(findViewById(R.id.toolbar))
-
-        lisFromDataBase()
-
         ctnContent = findViewById(R.id.ctn_content)
 
         // RecyclerView
@@ -81,64 +61,53 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun insertIntoDataBase(task: Task){
-        CoroutineScope(IO).launch{
-            dao.insert(task)
-            lisFromDataBase()
-        }
+    override fun onStart() {
+        super.onStart()
+        lisFromDataBase()
     }
 
-    private fun updateIntoDataBase(task: Task){
-        CoroutineScope(IO).launch{
-            dao.update(task)
-            lisFromDataBase()
-        }
+    private fun deleteAll() {
+        val taskAction = TaskAction(null,ActionType.DELETE_ALL.name)
+        viewModel.execute(taskAction)
     }
 
-    private  fun deleteAll(){
-        CoroutineScope(IO).launch {
-            dao.deleteAll()
-            lisFromDataBase()
+    private fun lisFromDataBase() { //inserir item list de tarefa da base de dados
+        //Observer
+        val listObserver = Observer<List<Task>> { listTasks ->//renomei o it
+            if (listTasks.isEmpty()){//imagem de estado vazio
+                ctnContent.visibility = View.VISIBLE
+            } else{
+                ctnContent.visibility = View.GONE
+            }
+            adapter.submitList(listTasks)
         }
+
+        //Live Data
+        viewModel.taskListLiveData.observe(this@TaskListActivity, listObserver)
     }
 
-    private  fun deleteById(id: Int){
-        CoroutineScope(IO).launch {
-            dao.deleteById(id)
-            lisFromDataBase()
-        }
+    private fun onListItemClicked(task: Task) {
+        openTaskListDetail(task)
     }
 
-    private fun lisFromDataBase(){ //inserir item list de tarefa da base de dados
-        CoroutineScope(IO).launch {
-            val myDataBaseList: List<Task> = dao.getAll()
-            adapter.submitList(myDataBaseList)
-        }
-    }
-
-    private fun showMessage(view: View, message: String){
-        Snackbar.make(view, message, Snackbar.LENGTH_LONG)
-            .setAction("Action", null)
-            .show()
-    }
-    private fun onListItemClicked(task: Task){
-       openTaskListDetail(task)
-    }
     private fun openTaskListDetail(task: Task?) {
         val intent = TaskDetailActivity.start(this, task)
         starForResult.launch(intent)
     }
+
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         val inflater: MenuInflater = menuInflater
-        inflater.inflate(R.menu.menu_task_list,menu)
+        inflater.inflate(R.menu.menu_task_list, menu)
         return true
     }
+
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when (item.itemId){
-            R.id.delete_all_task ->{ //Deletar todas as tarefas
+        return when (item.itemId) {
+            R.id.delete_all_task -> { //Deletar todas as tarefas
                 deleteAll()
                 true
             }
+
             else -> super.onOptionsItemSelected(item)
         }
     }
@@ -146,13 +115,15 @@ class MainActivity : AppCompatActivity() {
 
 enum class ActionType {
     DELETE,
+    DELETE_ALL,
     UPDATE,
     CREATE
 
 }
+
 data class TaskAction(
-    val task: Task,
+    val task: Task?,
     val ActionType: String
-): Serializable
+) : Serializable
 
 const val TASK_ACTION_RESULT = "TASK_ACTION_RESULT"
--- app/src/test/java/com/comunidadedevspace/taskbeats/ExampleUnitTest.kt
+++ app/src/test/java/com/comunidadedevspace/taskbeats/ExampleUnitTest.kt
@@ -2,7 +2,9 @@ package com.comunidadedevspace.taskbeats
 
 import org.junit.Test
 
-import org.junit.Assert.*
+import org.junit.Assert.assertEquals
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.whenever
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,8 +12,23 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+
+    private val mockNumbersProvider: MyNumbersProvider = mock()
+
+    private val underTest = MyCountRepositoryImpl(
+        numbersProvider = mockNumbersProvider
+    )
+
     @Test
     fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+        //Give
+        whenever(mockNumbersProvider.getNumber()).thenReturn(2)
+
+        //when
+        val result = underTest.sum()
+
+        //then
+        val expected = 4
+        assertEquals(expected, result)
     }
 }
